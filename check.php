bcef45a0e2b37236c80ce95af63516049c9d39ce
en00x <en00x@users.noreply.github.com>
2015-09-01 19:04:04 +0200

    Create check.php

---
+++ check.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<?php
+$cookie_name = "user";
+$cookie_value = "John Doe";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/", $_SERVER['SERVER_NAME']); // 86400 = 1 day
+echo $_SERVER['SERVER_NAME'];
+?>
+<html>
+<body>
+
+<?php
+if(count($_COOKIE) > 0) {
+    echo "Cookies are enabled.";
+} else {
+    echo "Cookies are disabled.";
+}
+?>
+
+<?php
+if(!isset($_COOKIE[$cookie_name])) {
+    echo "Cookie named '" . $cookie_name . "' is not set!";
+} else {
+    echo "Cookie '" . $cookie_name . "' is set!<br>";
+    echo "Value is: " . $_COOKIE[$cookie_name];
+}
+?>
+
+</body>
+</html> 
